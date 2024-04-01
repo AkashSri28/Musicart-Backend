8e62f944c20d0e59ecd8be230e0efb0537cb32da
Akash Srivastava <akasriva@adobe.com>
2024-04-02 00:02:56 +0530

    auth middleware removed

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { addToCart, getUserCart,updateCartItemQuantity } = require('../controllers/cartController');
-const authMiddleware = require('../middleware/authMiddleware');
 
 // POST route to add a product to the cart
 router.post('/add', addToCart);
