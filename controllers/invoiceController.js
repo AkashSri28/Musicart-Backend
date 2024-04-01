735629dd2f12b21ca4b1c945586063d12ac523f8
Akash Srivastava <akasriva@adobe.com>
2024-04-01 11:19:26 +0530

    Get invoice by id api

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -2,6 +2,21 @@ const Invoice = require('../models/InvoiceModel');
 const Cart = require('../models/CartModel');
 const mongoose = require('mongoose');
 
+const getInvoiceById = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const invoice = await Invoice.findById(id);
+      if (!invoice) {
+        return res.status(404).json({ error: 'Invoice not found' });
+      }
+      res.status(200).json(invoice);
+    } catch (error) {
+      console.error('Error fetching invoice:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+
 const getInvoices = async (req, res) => {
     try {
         // Retrieve user ID from authenticated request
@@ -43,4 +58,4 @@ const saveInvoice = async (req, res) => {
   
 
   
-  module.exports = {saveInvoice, getInvoices};
+  module.exports = {saveInvoice, getInvoices, getInvoiceById};
--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { saveInvoice, getInvoices } = require('../controllers/invoiceController');
+const { saveInvoice, getInvoices, getInvoiceById } = require('../controllers/invoiceController');
 const router = express.Router();
 
+// Route to get a invoice by ID
+router.get('/:id', getInvoiceById);
 
 //Router to save invoice
 router.post('/save', saveInvoice);
