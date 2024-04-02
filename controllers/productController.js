539b94072946caa2c4c6b5175b698e43ce76858f
Akash Srivastava <akasriva@adobe.com>
2024-04-02 07:29:41 +0530

    sort logic updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -20,9 +20,9 @@ const sortProducts = async (req, res) => {
 
     // Apply sorting based on the criteria
     if (criteria === 'priceLowest') {
-      products = await Product.find().sort({ $expr: { $toDouble: "$price" } });
+      products = await Product.find().sort({ price: parseFloat(1) });
     } else if (criteria === 'priceHighest') {
-      products = await Product.find().sort({ $expr: { $toDouble: "$price" } }).sort({ price: -1 });
+      products = await Product.find().sort({ price: parseFloat(-1) });
     } else if (criteria === 'nameAZ') {
       products = await Product.find().sort({ productName: 1 });
     } else if (criteria === 'nameZA') {
